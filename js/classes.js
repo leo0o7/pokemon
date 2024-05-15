ad7ff86893ae54aeed5827ee0a26c537e8e40173
leo0o7 <giardinoleonardo34@gmail.com>
2024-05-15 19:57:12 +0200

    (test): collision outline

--- js/classes.js
+++ js/classes.js
@@ -1,11 +1,42 @@
 import { c } from "./index.js";
 
 export class Sprite {
-  constructor({ pos, img }) {
+  constructor({ pos, vel, img, frames = { max: 1 } }) {
     this.pos = pos;
     this.img = img;
+    this.frames = frames;
+
+    this.img.onload = () => {
+      this.width = this.img.width / this.frames.max;
+      this.height = this.img.height;
+    };
   }
   draw() {
-    c.drawImage(this.img, this.pos.x, this.pos.y);
+    c.drawImage(
+      this.img,
+      0,
+      0,
+      this.img.width / this.frames.max,
+      this.img.height,
+      this.pos.x,
+      this.pos.y,
+      this.img.width / this.frames.max,
+      this.img.height
+    );
+  }
+}
+
+export class Boundary {
+  static width = 48;
+  static height = 48;
+  constructor({ pos }) {
+    this.pos = pos;
+    this.width = 48;
+    this.height = 48;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.pos.x, this.pos.y, this.width, this.height);
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-import { Sprite } from "./classes.js";
+import { Boundary, Sprite } from "./classes.js";
+import { collisions } from "./data/collisions.js";
 
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
@@ -10,6 +11,31 @@ c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 const center = { x: canvas.width / 2, y: canvas.height / 2 };
+
+const collisionsMap = [];
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, 70 + i));
+}
+
+const boundaries = [];
+const offset = {
+  x: -735,
+  y: -595,
+};
+collisionsMap.forEach((row, i) => {
+  row.forEach((val, j) => {
+    if (val === 1025) {
+      boundaries.push(
+        new Boundary({
+          pos: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+    }
+  });
+});
 
 const keys = {
   w: { pressed: false },
@@ -31,26 +57,58 @@ const bg = new Sprite({
   img: img,
 });
 
+const player = new Sprite({
+  pos: {
+    x: center.x - playerImage.width / 8,
+    y: center.y - playerImage.height / 2,
+  },
+  img: playerImage,
+  frames: {
+    max: 4,
+  },
+});
+
 function animate() {
   window.requestAnimationFrame(animate);
 
   bg.draw();
-  c.drawImage(
-    playerImage,
-    0,
-    0,
-    playerImage.width / 4,
-    playerImage.height,
-    center.x - playerImage.width / 8,
-    center.y - playerImage.height / 2,
-    playerImage.width / 4,
-    playerImage.height
-  );
+  player.draw();
 
-  if (keys.w.pressed && lastKey === "w") bg.pos.y += 3;
-  if (keys.a.pressed && lastKey === "a") bg.pos.x += 3;
-  if (keys.s.pressed && lastKey === "s") bg.pos.y -= 3;
-  if (keys.d.pressed && lastKey === "d") bg.pos.x -= 3;
+  // draw boundaries (TESTING)
+  boundaries.forEach((boundary) => {
+    boundary.draw();
+  });
+
+  if (keys.w.pressed && lastKey === "w") {
+    bg.pos.y += 12;
+    boundaries.forEach((boundary) => {
+      boundary.pos.y += 12;
+    });
+    keys.w.pressed = false;
+  }
+  if (keys.a.pressed && lastKey === "a") {
+    bg.pos.x += 12;
+    boundaries.forEach((boundary) => {
+      boundary.pos.x += 12;
+    });
+    keys.a.pressed = false;
+  }
+  if (keys.s.pressed && lastKey === "s") {
+    bg.pos.y -= 12;
+    boundaries.forEach((boundary) => {
+      boundary.pos.y -= 12;
+    });
+
+    keys.s.pressed = false;
+  }
+  if (keys.d.pressed && lastKey === "d") {
+    bg.pos.x -= 12;
+    boundaries.forEach((boundary) => {
+      boundary.pos.x -= 12;
+    });
+
+    keys.d.pressed = false;
+  }
 }
 animate();
 
